3ebf3665a5ce3fdcaab58e63149911cd224ba10c
Julia Dahm <jukabrown@gmail.com>
2017-07-12 19:58:53 -0400

    reorganized cors in middleware to actually work

--- src/services/weather.service.js
+++ src/services/weather.service.js
@@ -1,14 +1,20 @@
 const weatherData = require('../../mocks/weather.json');
-WeatherService.$inject = [];
+WeatherService.$inject = ['$http'];
 //use this if a dependency is needed, empty for now
 
-function WeatherService(){
+function WeatherService($http){
+  const baseUrl = "https://gentle-caverns-52020.herokuapp.com/weather/29,-81"
+  //variable must be defined before returned or it will return before using this
   return {
     //method label     :   function name
     getCurrently: getCurrentlyFunction //giving the label getCurrently the function getCurrentlyFunction
   }
   function getCurrentlyFunction(){
-    return weatherData.currently;
+    return $http.get(baseUrl)
+                .then(response => { //response is a var name for whatever comes back from the get request
+                  console.log(response);
+                  return response.data.currently;
+                })
   }
 }
 //this whole function grabs the currently information for you then the parent function will return it
