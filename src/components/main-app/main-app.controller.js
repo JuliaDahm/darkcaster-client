e360d505d63201f22b26fb725e413f8d07440e47
Julia Dahm <jukabrown@gmail.com>
2017-07-24 20:34:46 -0400

    Added search functionaity to main app controller and removed unnecessary passing in of images

--- src/components/main-app/main-app.controller.js
+++ src/components/main-app/main-app.controller.js
@@ -1,8 +1,15 @@
-MainAppController.$inject = ['WeatherService', 'images'];
+MainAppController.$inject = ['WeatherService'];
 
-function MainAppController(weather, images){
+function MainAppController(weather){
   this.lat = 0;
   this.lon = 0;
+  this.search = search;
+
+  function search(){
+    console.log('searching...');
+    weather.getAll(this.lat, this.lon)
+           .this(weatherData => this.weatherData = weatherData);
+  }
 }
 
 module.exports = MainAppController;
