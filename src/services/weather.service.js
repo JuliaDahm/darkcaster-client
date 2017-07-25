9c4664c5b80fe33a56604f693f10fa342c7627be
Julia Dahm <jukabrown@gmail.com>
2017-07-24 20:55:03 -0400

    Removed redundant methods and mockup

--- src/services/weather.service.js
+++ src/services/weather.service.js
@@ -1,4 +1,3 @@
-const weatherData = require('../../mocks/weather.json');
 WeatherService.$inject = ['$http'];
 //use this if a dependency is needed, empty for now
 
@@ -7,11 +6,7 @@ function WeatherService($http){
   //variable must be defined before returned or it will return before using this
   return {
     //method label     :   function name
-    getAll: getAllFunction,
-    getCurrently: getCurrentlyFunction, //giving the label getCurrently the function getCurrentlyFunction
-    getHourly: getHourlyFunction,
-    getMinutely: getMinutelyFunction,
-    getDaily: getDailyFunction
+    getAll: getAllFunction
   };
   function getAllFunction(lat, lon){
     const url = `${baseUrl}${lat},${lon}`;
@@ -19,38 +14,6 @@ function WeatherService($http){
                 .then(response => {
                   console.log(response);
                   return response.data;
-                });
-  }
-  function getCurrentlyFunction(lat, lon){
-    const url = `${baseUrl}${lat},${lon}`;
-    return $http.get(url)
-                .then(response => { //response is a var name for whatever comes back from the get request
-                  console.log(response);
-                  return response.data.currently;
-                });
-  }
-  function getHourlyFunction(lat, lon){
-    const url = `${baseUrl}${lat},${lon}`;
-    return $http.get(url)
-                    .then(response => {
-                      console.log(response);
-                      return response.data.hourly;
-                    });
-  }
-  function getMinutelyFunction(lat, lon){
-    const url = `${baseUrl}${lat},${lon}`;
-    return $http.get(url)
-                .then(response => {
-                  console.log(response);
-                  return response.data.minutely;
-                });
-  }
-  function getDailyFunction(lat, lon){
-    const url = `${baseUrl}${lat},${lon}`;
-    return $http.get(url)
-                .then(response => {
-                  console.log(response);
-                  return response.data.daily;
                 });
   }
 }
